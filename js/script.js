22956445d68bf66fad0202ffade7befb0d60fad7
valerie_l <contacttovalerie@gmail.com>
2023-04-09 23:21:51 -0400

    Added pet status display

--- js/script.js
+++ js/script.js
@@ -24,15 +24,35 @@ const createPet = function (name, species) {
   return pet;
 };
 
-const sora = createPet("Sora, ferret");
-const clover = createPet("Clover, rabbit");
-const baxter = createPet("Baxter, hamster");
-const cleo = createPet("Cleo, rat");
-const francine = createPet("Francine, turtle");
+const sora = createPet("Sora", "ferret");
+const clover = createPet("Clover", "rabbit");
+const baxter = createPet("Baxter", "hamster");
+const cleo = createPet("Cleo", "rat");
+const francine = createPet("Francine", "turtle");
 
 //console.log(sora, clover, baxter, cleo, francine);
 
-clover.sleep();
-baxter.play();
+clover.isTired = 8;
+francine.isTired = 9;
 
-console.log(clover, baxter);
+const allPets = [sora, clover, baxter, cleo, francine];
+
+//console.log(allPets);
+
+const showPets = function (petArray) {
+  pets.innerHTML = "";
+
+  for (let pet of petArray) {
+    let status = "ready to play!";
+    if (pet.isTired >= 7) {
+      status = "sleeping.";
+    }
+    const li = document.createElement("li");
+    li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+    pets.append(li);
+  }
+};
+
+statusButton.addEventListener("click", function () {
+  showPets(allPets);
+});
